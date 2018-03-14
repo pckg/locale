29c87c52f9d1d80a72addfed594cb7318a5675d9
Bojan Rajh <schtr4jh@schtr4jh.net>
2018-03-14 20:40:12 +0100

    Handle console language

--- src/Pckg/Locale/Middleware/InitRequestLanguage.php
+++ src/Pckg/Locale/Middleware/InitRequestLanguage.php
@@ -7,46 +7,44 @@ class InitRequestLanguage
 
     public function execute(callable $next)
     {
-        /**
-         * Check for locale settings.
-         */
-        if (!isHttp()) {
-            return $next();
-        }
-
-        $match = request()->getMatch();
-        $langCode = $match['language'] ?? null;
-
-        if ($langCode) {
-            message('Setting language from route match.');
-            $this->setFromLangCode($langCode);
-
-            return $next();
-        }
-
-        /**
-         * Try to get language from previous request.
-         */
-        /*if ($referer = server('HTTP_REFERER', null)) {
-            $url = parse_url($referer);
-            $match = (new ResolveRoute(router(), $url['path'],
-                                       first($url['host'], server('HTTP_HOST'), config('domain'))))->execute();
+        if (isHttp()) {
+            /**
+             * Check for http stuff.
+             */
+            $match = request()->getMatch();
             $langCode = $match['language'] ?? null;
 
             if ($langCode) {
+                message('Setting language from route match.');
                 $this->setFromLangCode($langCode);
 
                 return $next();
             }
-        }*/
 
-        $domain = server('HTTP_HOST');
-        $language = Language::gets(['domain' => $domain]);
-        if ($language) {
-            message('Setting language from domain.');
-            $language->setAsCurrent();
+            /**
+             * Try to get language from previous request.
+             */
+            /*if ($referer = server('HTTP_REFERER', null)) {
+                $url = parse_url($referer);
+                $match = (new ResolveRoute(router(), $url['path'],
+                                           first($url['host'], server('HTTP_HOST'), config('domain'))))->execute();
+                $langCode = $match['language'] ?? null;
 
-            return $next();
+                if ($langCode) {
+                    $this->setFromLangCode($langCode);
+
+                    return $next();
+                }
+            }*/
+
+            $domain = server('HTTP_HOST');
+            $language = Language::gets(['domain' => $domain]);
+            if ($language) {
+                message('Setting language from domain.');
+                $language->setAsCurrent();
+
+                return $next();
+            }
         }
 
         /**
