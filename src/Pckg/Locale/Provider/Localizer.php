a62082d5669ee469cd2b5be18ee441798870cbc5
Bojan Rajh <schtr4jh@schtr4jh.net>
2018-08-08 18:23:37 +0200

    Invalidate language cache

--- src/Pckg/Locale/Provider/Localizer.php
+++ src/Pckg/Locale/Provider/Localizer.php
@@ -1,7 +1,9 @@
 <?php namespace Pckg\Locale\Provider;
 
+use Pckg\Cache\Handler\InvalidateCacheByRecord;
 use Pckg\Framework\Provider;
 use Pckg\Locale\Console\PrepareLocalizer;
+use Pckg\Locale\Record\Language;
 
 class Localizer extends Provider
 {
@@ -10,6 +12,21 @@ class Localizer extends Provider
     {
         return [
             PrepareLocalizer::class,
+        ];
+    }
+
+    public function listeners()
+    {
+        return [
+            Language::class . '.inserted' => [
+                InvalidateCacheByRecord::class,
+            ],
+            Language::class . '.updated'  => [
+                InvalidateCacheByRecord::class,
+            ],
+            Language::class . '.deleted'  => [
+                InvalidateCacheByRecord::class,
+            ],
         ];
     }
 
