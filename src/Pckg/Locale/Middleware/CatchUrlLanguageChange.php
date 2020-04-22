13125130edd31d88cb6341db896e61d38462fed7
Bojan Rajh <schtr4jh@schtr4jh.net>
2020-04-22 19:36:20 +0200

    Catch locale change on backend only

--- src/Pckg/Locale/Middleware/CatchUrlLanguageChange.php
+++ src/Pckg/Locale/Middleware/CatchUrlLanguageChange.php
@@ -5,13 +5,25 @@ class CatchUrlLanguageChange
 
     public function execute(callable $next)
     {
-        if ($lang = get('lang')) {
-            /**
-             * @T00D00 - check lang existance
-             */
-            $_SESSION['pckg_dynamic_lang_id'] = get('lang');
-            redirect();
+        if (!isHttp()) {
+            return $next();
         }
+
+        $lang = get('lang');
+        if (!$lang) {
+            return $next();
+        }
+
+        $router = router();
+        if (!in_array('layout:backend', $router->get('tags'))) {
+            return $next();
+        }
+
+        /**
+         * @T00D00 - keep language id.
+         */
+        $_SESSION['pckg_dynamic_lang_id'] = get('lang');
+        redirect();
 
         return $next();
     }
